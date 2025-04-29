8888a428702c451e27fd1fe59bd29998ef32f539
saad ishfaq <saadtbc1309@gmail.com>
2025-04-29 23:09:43 +0500

    removed un nessary code

--- src/hooks/useGet.ts
+++ src/hooks/useGet.ts
@@ -1,18 +1,13 @@
 // hooks/useGet.ts
-import { useQuery, UseQueryOptions } from '@tanstack/react-query';
-import axios from 'axios';
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
-export function useGet<T>(
-  queryKey: string | unknown[],
-  url: string,
-  options?: Omit<UseQueryOptions<T>, 'queryKey' | 'queryFn'>
-) {
+export function useGet<T>(queryKey: string | unknown[], url: string) {
   return useQuery<T>({
-    queryKey: Array.isArray(queryKey) ? queryKey : [queryKey],
+    queryKey: queryKey as any,
     queryFn: async () => {
       const response = await axios.get<T>(url);
       return response.data;
     },
-    ...options,
   });
 }
